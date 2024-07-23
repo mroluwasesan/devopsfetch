1c86df038c828ff1e6360f1fddb1d2ee1e50fa96
mroluwasesan <mr.oluwasesan@gmail.com>
2024-07-23 12:19:51 +0100

    rotate logs

--- devopsfetch.sh
+++ devopsfetch.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-show_help() {
-    cat << HELP
-Usage: ${0##*/} [-h] [-p [port_number]] [-d [container_name]] [-n [domain]] [-u [username]] [-t [date_range]]
-Retrieve and display system information.
-
-    -h, --help              Display this help and exit.
-    -p, --port [port_number]Display all active ports and services, or detailed information about a specific port.
-    -d, --docker [name]     List all Docker images and containers, or detailed information about a specific container.
-    -n, --nginx [domain]    Display all Nginx domains and their ports, or detailed configuration information for a specific domain.
-    -u, --users [username]  List all users and their last login times, or detailed information about a specific user.
-    -t, --time [date_range] Display activities within a specified date range (YYYY-MM-DD or YYYY-MM-DD YYYY-MM-DD).
-HELP
-}
-
 list_ports() {
     echo -e "Netid\tState\tRecv-Q\tSend-Q\tLocal Address:Port\tPeer Address:Port\tProcess"
     ss -tulnp | awk 'NR>1 {print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6 "\t" $7}'
@@ -63,6 +49,22 @@ time_range_activities() {
     echo "Activities from $start_date to $end_date:"
     journalctl --since="$start_date" --until="$end_date"
 }
+
+
+show_help() {
+    cat << HELP
+Usage: ${0##*/} [-h] [-p [port_number]] [-d [container_name]] [-n [domain]] [-u [username]] [-t [date_range]]
+Retrieve and display system information.
+
+    -h, --help              Display this help and exit.
+    -p, --port [port_number]Display all active ports and services, or detailed information about a specific port.
+    -d, --docker [name]     List all Docker images and containers, or detailed information about a specific container.
+    -n, --nginx [domain]    Display all Nginx domains and their ports, or detailed configuration information for a specific domain.
+    -u, --users [username]  List all users and their last login times, or detailed information about a specific user.
+    -t, --time [date_range] Display activities within a specified date range (YYYY-MM-DD or YYYY-MM-DD YYYY-MM-DD).
+HELP
+}
+
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
